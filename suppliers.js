b028d77c2a0cbc0158952adccc4123504a4d2668
jbowring <joelbowring@me.com>
2021-07-03 11:14:19 +0100

    Allow multiple file types, change RS to include .htm

--- suppliers.js
+++ suppliers.js
@@ -2,19 +2,25 @@ const suppliers = {
     "digikey": {
         name: "DigiKey",
         type: "csv",
-        fileType: ".csv",
+        fileTypes: [
+            ".csv"
+        ],
         decimalPlaces: 5,
     },
     "farnell": {
         name: "Farnell",
         type: "csv",
-        fileType: ".csv",
+        fileTypes: [
+            ".csv"
+        ],
         decimalPlaces: 4,
     },
     "avnet": {
         name: "Avnet",
         type: "csv",
-        fileType: ".csv",
+        fileTypes: [
+            ".csv"
+        ],
         decimalPlaces: 4,
         config: {
             delimiter: '\t',
@@ -23,24 +29,31 @@ const suppliers = {
     "mouser": {
         name: "Mouser",
         type: "xls",
-        fileType: ".xls",
+        fileTypes: [
+            ".xls"
+        ],
         decimalPlaces: 3,
     },
     "rs": {
         name: "RS",
         type: "rs",
-        fileType: ".html",
+        fileTypes: [
+            ".htm",
+            ".html"
+        ],
         decimalPlaces: 5,
         instructions: [
             'Before checkout, navigate to your RS basket page',
-            'Download the page as a .html file (not .mhtml)',
+            'Download the page as a .htm or .html file (not .mhtml)',
             'Upload the file below',
         ]
     },
     "thorlabs": {
         name: "Thorlabs",
         type: "xls",
-        fileType: ".xls",
+        fileTypes: [
+            ".xls"
+        ],
         decimalPlaces: 2,
     },
 }
